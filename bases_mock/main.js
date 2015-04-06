c6187030bfc3a2d7e0da167aaf6473eb1b0e9fde
vietanh <mr.vietanh@gmail.com>
2015-04-05 22:04:34 -0700

    Implement up to inventory scene

--- bases_mock/main.js
+++ bases_mock/main.js
@@ -3,21 +3,31 @@ var numScenes = 0;
 var allSceneEls = $('.scene');
 var bodyEl = $('body');
 
+bodyEl.ready(main);
+
 function main() {
   numScenes = allSceneEls.length;
   bodyEl.click(function(evt) {
     var isNext = evt.clientX > bodyEl.width() / 3; 
     nextScene(isNext);
   });
+  if (window.location.search) {
+    var sceneIndexParam = window.location.search.split('=')[1];
+    gotoScene(sceneIndexParam);
+  }
 }
 
 function nextScene(isNext) {
-  var currentIndex = allSceneEls.index('.showing');
+  var currentIndex = allSceneEls.filter('.showing').index();
   var nextIndex = isNext ?
       (currentIndex + 1) % numScenes :
       (currentIndex + numScenes - 1) % numScenes;
   $(allSceneEls[currentIndex]).removeClass('showing');
   $(allSceneEls[nextIndex]).addClass('showing');
+  window.location.search = 'scene=' + nextIndex;
 }
 
-main();
+function gotoScene(sceneIndex) {
+  allSceneEls.removeClass('showing');
+  $(allSceneEls[sceneIndex]).addClass('showing');
+}
